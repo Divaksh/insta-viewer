32a4055bb79bb824b3bf960173f92c039e441a14
Divaksh <me@divaksh.me>
2020-09-07 03:38:52 +0530

    Added profile related funtionality to header

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -5,6 +5,10 @@ import Toolbar from "@material-ui/core/Toolbar";
 import InputBase from "@material-ui/core/InputBase";
 import { makeStyles } from "@material-ui/core/styles";
 import SearchIcon from "@material-ui/icons/Search";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuItem from "@material-ui/core/MenuItem";
+import Popover from "@material-ui/core/Popover";
 import { Link } from "react-router-dom";
 
 // Custom Styles to over ride material ui default styles
@@ -44,14 +48,43 @@ const useStyles = makeStyles((theme) => ({
       },
     },
   },
+  avatar: {
+    width: 40,
+    height: 40,
+  },
   header: {
     backgroundColor: "#263238",
+  },
+  hr: {
+    height: "1.5px",
+    backgroundColor: "#f2f2f2",
+    marginLeft: "5px",
+    marginRight: "5px",
   },
 }));
 
 const Header = ({ state }) => {
   //store custom Styles in classes
   const classes = useStyles();
+  const [setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleAccount = () => {
+    handleAccount();
+    handleClose();
+  };
+
+  const handleLogout = () => {
+    handleLogout();
+    handleClose();
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div>
@@ -80,6 +113,42 @@ const Header = ({ state }) => {
                   input: classes.inputInput,
                 }}
               />
+            </div>
+          )}
+          {(state.isHome || state.isProfile) && (
+            <div>
+              <IconButton onClick={handleClick}>
+                <Avatar
+                  alt="Profile Pic"
+                  src={state.userProfileUrl}
+                  className={classes.avatar}
+                  style={{ border: "1px solid #fff" }}
+                />
+              </IconButton>
+              <Popover
+                id="simple-menu"
+                anchorEl={state.anchorEl}
+                open={Boolean(state.anchorEl)}
+                onClose={handleClose}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+              >
+                <div style={{ padding: "5px" }}>
+                  {state.isHome && (
+                    <div>
+                      <MenuItem onClick={handleAccount}>My Account</MenuItem>
+                      <div className={classes.hr} />
+                    </div>
+                  )}
+                  <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                </div>
+              </Popover>
             </div>
           )}
         </Toolbar>
