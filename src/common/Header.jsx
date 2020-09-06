0944b6ef9cfcae58bb15898fbf8d74a4cac59136
Divaksh <me@divaksh.me>
2020-09-07 03:31:36 +0530

    Updated Header component from static to dynamic - Made it functional component as it doesn't require state - Added material App Bar - Added dynamic logo logic

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 import "./Header.css";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import InputBase from "@material-ui/core/InputBase";
+import { makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuItem from "@material-ui/core/MenuItem";
+import Popover from "@material-ui/core/Popover";
+import { Link } from "react-router-dom";
 
-const Header = () => {
+// Custom Styles to over ride material ui default styles
+
+const useStyles = makeStyles((theme) => ({
+  header: {
+    backgroundColor: "#263238",
+  },
+}));
+
+const Header = ({ state }) => {
+  //store custom Styles in classes
+  const classes = useStyles();
+
   return (
-    <div className="header">
-      <span className="logoTitle">Image Viewer</span>
+    <div>
+      <AppBar className={classes.header}>
+        <Toolbar>
+          {(state.isLogin || state.isHome) && (
+            <span className="logoTitle">Image Viewer</span>
+          )}
+          {state.isProfile && (
+            <Link style={{ textDecoration: "none", color: "white" }} to="/home">
+              <span className="logoTitle">Image Viewer</span>
+            </Link>
+          )}
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
