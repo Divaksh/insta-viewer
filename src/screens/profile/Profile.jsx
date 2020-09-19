f93614127f0df32dc058a202f5b617d2527e5bac
Divaksh <me@divaksh.me>
2020-09-20 00:58:59 +0530

    Added about section in the profile component - Added statics follows, followed by and posts count - Name edit functionality added

--- src/screens/profile/Profile.jsx
+++ src/screens/profile/Profile.jsx
@@ -4,6 +4,20 @@ import "../../common/Common.css";
 import "./Profile.css";
 import { withRouter } from "react-router";
 import axios from "axios";
+import ProfilePic from "../../assets/ProfilePic.jpg";
+
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import FormControl from "@material-ui/core/FormControl";
+import Button from "@material-ui/core/Button";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import Container from "@material-ui/core/Container";
+import Fab from "@material-ui/core/Fab";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import Modal from "@material-ui/core/Modal";
+import Grid from "@material-ui/core/Grid";
+import EditIcon from "@material-ui/icons/Edit";
 
 class Profile extends Component {
   constructor(props) {
@@ -16,6 +30,10 @@ class Profile extends Component {
       follows: "10M",
       followedby: "1",
       name: "Divaksh Jain",
+      openNameEditModal: false,
+      closeNameEditModal: true,
+      newName: "",
+      nameRequired: false,
     };
   }
 
@@ -64,10 +82,148 @@ class Profile extends Component {
     return (
       <>
         <Header state={this.state} />
-        <div className="mainContainer"></div>
+        <div className="mainContainer">
+          <Container>
+            <Grid container spacing={3} justify="flex-start">
+              <Grid item xs={2} />
+              <Grid item xs={2}>
+                {ProfilePic ? (
+                  <Avatar
+                    alt="profile_pic"
+                    id="profile-pic"
+                    variant="circle"
+                    src={ProfilePic}
+                  />
+                ) : null}
+              </Grid>
+              <Grid item xs={5} id="user_name">
+                <Typography
+                  variant="h4"
+                  component="h1"
+                  style={{ marginBottom: 5 }}
+                >
+                  {this.state.mediaData ? this.state.username : null}
+                </Typography>
+                <Grid container spacing={3} justify="center">
+                  <Grid item xs={4}>
+                    Posts: {this.state.posts ? this.state.posts : null}
+                  </Grid>
+                  <Grid item xs={4}>
+                    Follows: {this.state.follows ? this.state.follows : null}
+                  </Grid>
+                  <Grid item xs={4}>
+                    Followed By:{" "}
+                    {this.state.followedby ? this.state.followedby : null}
+                  </Grid>
+                </Grid>
+                <Typography
+                  variant="h6"
+                  component="h2"
+                  style={{ marginTop: 10 }}
+                >
+                  {this.state.name ? this.state.name : null}
+                  {this.state.mediaData && !this.state.name
+                    ? this.state.mediaData.name
+                    : null}
+                  <Fab
+                    color="secondary"
+                    id="edit-profile"
+                    aria-label="edit"
+                    onClick={this.openEditNameModal}
+                  >
+                    <EditIcon fontSize="small" />
+                  </Fab>
+                </Typography>
+
+                <Modal
+                  open={this.state.openNameEditModal}
+                  onClose={this.closeEditNameModal}
+                >
+                  <div className="edit-modal-div">
+                    <h2>Edit</h2>
+                    <FormControl required>
+                      <InputLabel htmlFor="Name">Full Name</InputLabel>
+                      <Input
+                        id="fullName"
+                        type="text"
+                        onChange={this.onChangeEditName}
+                      />
+                      {this.state.nameRequired ? (
+                        <FormHelperText>
+                          <span style={{ color: "red" }}>required</span>
+                        </FormHelperText>
+                      ) : null}
+                    </FormControl>
+                    <div style={{ marginTop: 15 }}>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={this.onUpdateName}
+                      >
+                        UPDATE
+                      </Button>
+                    </div>
+                  </div>
+                </Modal>
+              </Grid>
+              <Grid item xs={4} />
+            </Grid>
+          </Container>
+        </div>
       </>
     );
   }
+
+  openEditNameModal = () => {
+    this.setState({
+      openNameEditModal: true,
+      closeNameEditModal: false,
+    });
+  };
+
+  closeEditNameModal = () => {
+    this.setState({
+      openNameEditModal: false,
+      closeNameEditModal: true,
+    });
+  };
+
+  onChangeEditName = (e) => {
+    if (e.target.value === "") {
+      this.setState({ newName: e.target.value, nameRequired: true });
+    } else {
+      this.setState({ newName: e.target.value, nameRequired: false });
+    }
+  };
+
+  onUpdateName = () => {
+    if (this.state.newName.trim() === "") {
+      this.setState({
+        nameRequired: true,
+      });
+    } else {
+      this.setState({
+        name: this.state.newName,
+        nameRequired: false,
+        newName: "",
+      });
+      this.closeEditNameModal();
+    }
+  };
+
+  openEditNameModal = () => {
+    this.setState({
+      openNameEditModal: true,
+      closeNameEditModal: false,
+    });
+  };
+
+  closeEditNameModal = () => {
+    this.setState({
+      openNameEditModal: false,
+      closeNameEditModal: true,
+    });
+  };
 }
 
 export default withRouter(Profile);
